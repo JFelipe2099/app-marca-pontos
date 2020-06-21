db0230839118d2e06e45019acc0cd01956ad51aa
João Felipe <joaofelipe2099@hotmail.com>
2020-06-21 15:59:12 -0300

    Limpeza do código e atualização da fonte

--- app.js
+++ app.js
@@ -1,46 +1,17 @@
-// WORKS, BUT FOR ONLY ONE
-// const felipe = document.querySelector('.felipe');
-// const joao = document.querySelector('.joao');
-
-// let score = 0;
-// function add(pl){
-//     score++;
-//     pl.innerText = score;
-// }
-
-// function minus(){
-//     score--;
-//     felipe.innerText = score;
-// }
-
-// const players = document.querySelectorAll('.player');
+// engine do app
 const mesa = document.querySelector('.mesa');
 
 const addJogador = document.querySelector('.addJogador');
 const nomeJogador = document.querySelector('.nomeJogador');
 const novoJogador = document.querySelector('.novoJogador');
 
-// let score = 0;
-// players.forEach(player => player.addEventListener('click', event => {
-    
-//     if (event.target.tagName === 'BUTTON'){
-//         console.log(event.target);
-
-//         if (event.target.className === 'score'){
-//             // score++;
-//             player.querySelector('.score').innerText = Number(player.querySelector('.score').innerText) + 1;
-//         } else{
-//             // score--;
-//             player.querySelector('.score').innerText = Number(player.querySelector('.score').innerText) - 1;
-//         }
-//     }
-// }));
-
+// cores
+const color = ['#0569BE', '#C71C24', '#019C42', '#F3C600', '#ed79ed'];
 let colorSelector = 0;
+
+// adiciona um novo jogador a mesa
 novoJogador.addEventListener('click', event => {
     if (event.target.className === 'addJogador'){
-
-        const color = ['#0569BE', '#C71C24', '#019C42', '#F3C600', '#ed79ed'];
 
         const nome = nomeJogador.value;
         const newPlayer = document.createElement('div');
@@ -54,25 +25,16 @@ novoJogador.addEventListener('click', event => {
         if (colorSelector === 5){
             colorSelector = 0;
         }
-        // const newPlayer = `
-        //     <div class="player" style="background-color: #019C42">
-        //     <p>${nome}</p>
-        //     <button class="minus">&minus;</button>
-        //     <button class="score">0</button>
-        //     </div>
-        // `;
 
         mesa.append(newPlayer);
         nomeJogador.value = '';
-        // mesa.innerHTML += newPlayer;
 
+        // adiciona um event listener ao novo jogador criado
         newPlayer.addEventListener('click', event => {
             if (event.target.tagName === 'BUTTON'){
                 if (event.target.className === 'score'){
-                // score++;
                 newPlayer.querySelector('.score').innerText = Number(newPlayer.querySelector('.score').innerText) + 1;
             } else{
-                // score--;
                 newPlayer.querySelector('.score').innerText = Number(newPlayer.querySelector('.score').innerText) - 1;
             }
         }
