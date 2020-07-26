789236e13612b257d1483a11178129d5a7b12be2
João Felipe <joaofelipe2099@hotmail.com>
2020-07-25 23:10:46 -0300

    Atualizando a feature de log

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,17 +1,17 @@
 const btn = document.querySelector('.log');
-const popup = document.querySelector('.popup-wrapper');
+const popup = document.querySelectorAll('.popup-wrapper');
 const x = document.querySelector('.popup-close');
 
 btn.addEventListener('click', () => {
-    popup.style.display = 'block';
+    popup[0].style.display = 'block';
 });
 
 x.addEventListener('click', () => {
-    popup.style.display = 'none';
+    popup[0].style.display = 'none';
 });
 
-const closePopup = () => {
-    popup.style.display = 'none';
+const closePopup = (x=0) => {
+    popup[x].style.display = 'none';
 }
 
 // popup.addEventListener('click', () => {
